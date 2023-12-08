69a933a9309e1eaa38aedc7980c37cec5170be9e
David Linke <david.linke@catalysis.de>
2023-12-08 10:00:05 +0100

    Fix descriptions to consistently use pid4cat

--- src/pid4cat_model/datamodel/pid4cat_model.py
+++ src/pid4cat_model/datamodel/pid4cat_model.py
@@ -3,7 +3,7 @@
 # Schema: pid4cat-model
 #
 # id: https://w3id.org/nfdi4cat/pid4cat-model
-# description: LinkML model for Catalyst Resource Identifier (PID4Cat). PID4Cat is handle system based persistent identifier (PID) for digital or physical resources used in the catalysis research process. The handle record is used to store metadata about the PID besides the obligatory redirect URL.
+# description: LinkML model for PIDs for resources in catalysis(PID4Cat). PID4Cat is handle system based persistent identifier (PID) for digital or physical resources used in the catalysis research process. The handle record is used to store metadata about the PID besides the obligatory redirect URL.
 #   The model define here describes metadata for the PID itself and how to access the identified resource. It does not describe the resource itself with the exception of the resource category, which is a high-level description of  what is identified by the PID4Cat, e.g. sample or device.
 # license: MIT
 
