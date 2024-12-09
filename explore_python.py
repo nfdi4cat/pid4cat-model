e39b2090f418643a128c8d2de36976d88bbcaab7
David Linke <david.linke@catalysis.de>
2024-12-09 14:00:35 +0100

    Update python/pydantic examples

--- explore_python.py
+++ explore_python.py
@@ -1,43 +1,72 @@
 from linkml_runtime.dumpers import json_dumper
 from pid4cat_model.datamodel import pid4cat_model as p4c
 
+# Demonstrate the use of Python DataClass-based model
 
 p1_Agent = p4c.Agent(
-    orcid="0000-0000-0000-0000",
     name="Data Fuzzi",
-    role="TRUSTEE",
+    email="data.fuzzi@example.org",
+    orcid="0000-0000-0000-0000",
+    affiliation_ror="https://ror.org/01abcde",
+    role=p4c.PID4CatAgentRole.TRUSTEE,
 )
 
-p1_log = p4c.LogRecord(
-    datetime_log="2024-02-19T00:00:00Z",
-    has_agent=p1_Agent,
-    changed_field=p4c.ChangeLogField.STATUS,  # "STATUS",
-    description="as requested in issue #234",
-)
+p1_related = [
+    p4c.PID4CatRelation(
+        relation_type=p4c.RelationType.IS_PART_OF,
+        related_identifier="https://example.org/related",
+        datetime_log="2024-02-19T00:00:00Z",
+        has_agent=p1_Agent,
+    ),
+    p4c.PID4CatRelation(
+        relation_type=p4c.RelationType.IS_REFERENCED_BY,
+        related_identifier="https://example.org/referenced",
+        datetime_log="2024-02-19T00:00:00Z",
+        has_agent=p1_Agent,
+    ),
+]
 
-p1_resource_info = p4c.ResourceInfo(
+p1_repr_variants = [
+    p4c.RepresentationVariant(
+        url="https://example.org/resource",
+        media_type="text/turtle",
+        encoding_format="UTF-8",
+        size=12345,
+    ),
+    # could be more than one representation variant
+]
+
+p1_res_info = p4c.ResourceInfo(
     label="Resource label",
     description="Resource description",
     resource_category=p4c.ResourceCategory.SAMPLE,
-    representation_variants = [
-        p4c.RepresentationVariant(
-            url="https://example.org/resource",
-            media_type="text/turtle",
-            encoding_format="UTF-8",
-            size=12345,
-        ),
-    ],
+    representation_variants=p1_repr_variants,
 )
+
+p1_log = [
+    p4c.LogRecord(
+        datetime_log="2024-02-19T00:00:00Z",
+        has_agent=p1_Agent,
+        changed_field=p4c.ChangeLogField.STATUS,
+        description="Registration completed.",
+    ),
+    p4c.LogRecord(
+        datetime_log="2024-05-15T15:51:15Z",
+        has_agent=p1_Agent,
+        changed_field=p4c.ChangeLogField.RESOURCE_INFO,
+        description="as requested in issue #234",
+    ),
+]
 
 p1 = p4c.PID4CatRecord(
     id="lik-1",
-    change_log=p1_log,
     landing_page_url="https://pid4cat.example.org/lik-1",
-    status=p4c.PID4CatStatus.REGISTERED,  # "REGISTERED",
-    resource_info=p1_resource_info,
-    related_identifiers=None,
+    pid_schema_version="0.1.0",
     license="CC0-1.0",
     curation_contact_email="datafuzzi@example.org",
+    resource_info=p1_res_info,
+    related_identifiers=p1_related,
+    change_log=p1_log,
 )
 
 print(p1)
