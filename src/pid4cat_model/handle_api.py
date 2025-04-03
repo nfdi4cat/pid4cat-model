7e3905d314ecf85770bab85a8fa41e57ba66bcfe
David Linke <david.linke@catalysis.de>
2025-04-03 11:39:41 +0200

    Remove temporary data fixing code. Configure logging.

--- src/pid4cat_model/handle_api.py
+++ src/pid4cat_model/handle_api.py
@@ -10,7 +10,6 @@ from pid4cat_model.datamodel.pid4cat_model_pydantic import (
     Pid4CatRecord,
 )
 
-
 logger = logging.getLogger(__name__)
 
 # Regex to check if a string could be a json array [...] or json object {...}
@@ -28,16 +27,6 @@ class HandleConfig:
         self.base_url = f"{self.api_url}{self.prefix}/{self.ns_suffix}/"
 
 
-# TODO Removed function after the record is corrected.
-def fix_record_bug(parsed):
-    """Fix media_type in the metadata"""
-    if "representation_variants" in parsed:
-        for i, el in enumerate(parsed["representation_variants"]):
-            if el.get("media_type", "") == "textturtle":
-                parsed["representation_variants"][i]["media_type"] = "text/turtle"
-    return parsed
-
-
 class HandleNetAPI:
     def __init__(self, config: HandleConfig):
         self.config = config
@@ -49,15 +38,12 @@ class HandleNetAPI:
         response = self.client.get(url)
         data = response.json()
         logger.debug(f"Response: {data}")
-        # decode strings containing json
+        # decode records that contain escaped json syntax
         for rec in data.get("values", []):
             value = rec["data"]["value"].strip()
             if json_str.match(value):
                 try:
                     parsed = json.loads(value)
-                    # TODO Removed after the record is corrected.
-                    if id_suffix == "KBKA-BEM9":
-                        parsed = fix_record_bug(parsed)
                     rec["data"]["value"] = parsed
                 except json.JSONDecodeError as e:
                     print(f"Invalid json! Error: {e}")
@@ -69,6 +55,9 @@ class Pid4CatMetaData(Pid4CatRecord):
 
 
 if __name__ == "__main__":
+    # Set logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    logging.basicConfig(level=logging.INFO)
+
     config = HandleConfig()
     print(config.base_url)
     api = HandleNetAPI(config)
