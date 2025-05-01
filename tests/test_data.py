f12a278d975d186ad005cd11e4c23534ef316ab2
David Linke <dr.david.linke@gmail.com>
2025-05-01 19:26:29 +0200

    Fix data test and convert to pytest

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,30 +1,26 @@
-"""Data test."""
-
 import os
 import glob
-import unittest
 
 from linkml_runtime.loaders import yaml_loader, json_loader
-from pid4cat_model.datamodel.pid4cat_model import HandleRecordContainer
+from pid4cat_model.datamodel.pid4cat_model import HandleAPIRecord, HandleRecordContainer
 
 ROOT = os.path.join(os.path.dirname(__file__), "..")
-DATA_DIR = os.path.join(ROOT, "src", "data", "examples", "valid")
-
-EXAMPLE_FILES_YAML = glob.glob(os.path.join(DATA_DIR, "*.yaml"))
-EXAMPLE_FILES_JSON = glob.glob(os.path.join(DATA_DIR, "*.json"))
+DATA_DIR = os.path.join(ROOT, "tests", "data", "valid")
 
 
-class TestData(unittest.TestCase):
-    """Test data and datamodel."""
+def test_yaml_data():
+    """Yaml data test."""
+    for path in glob.glob(os.path.join(DATA_DIR, "HandleAPIRecord*.yaml")):
+        obj = yaml_loader.load(path, target_class=HandleAPIRecord)
+        assert obj
 
-    def test_yaml_data(self):
-        """Yaml data test."""
-        for path in EXAMPLE_FILES_YAML:
-            obj = yaml_loader.load(path, target_class=HandleRecordContainer)
-            assert obj
+    for path in glob.glob(os.path.join(DATA_DIR, "HandleRecordContainer*.yaml")):
+        obj = yaml_loader.load(path, target_class=HandleRecordContainer)
+        assert obj
 
-    def test_json_data(self):
-        """Json data test."""
-        for path in EXAMPLE_FILES_JSON:
-            obj = json_loader.load(path, target_class=HandleRecordContainer)
-            assert obj
+
+def test_json_data():
+    """Json data test."""
+    for path in glob.glob(os.path.join(DATA_DIR, "HandleAPIRecord*.json")):
+        obj = json_loader.load(path, target_class=HandleAPIRecord)
+        assert obj
